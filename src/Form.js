1341f378cc9786ed4e9d273b4c6e76d3765f2ef6
paulmatheson <iampaulmatheson@gmail.com>
2023-10-24 12:10:15 -0700

    Initializing project.

---
+++ src/Form.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+function Form() {
+    return (
+        "Form"
+    )
+}
+
+export default Form
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import './style.css'
+import desktopImg from './images/illustration-sign-up-desktop.svg'
+import mobileImg from './images/illustration-sign-up-mobile.svg'
+
+function Header() {
+    return (
+        <header className='App-header'>
+            Header
+        </header>
+    )
+}
+
+export default Header
