693ad7440c2dc6d8ba5afe7e4477e402f93b152c
paulmatheson <iampaulmatheson@gmail.com>
2023-10-29 19:47:15 -0700

    Laid the foundation for the submission message.

--- src/AppBody.js
+++ src/AppBody.js
@@ -1,23 +1,51 @@
-import './style.css'
-import Icon from './images/icon-list.svg'
+import React, { useState } from 'react';
+import './style.css';
+import Icon from './images/icon-list.svg';
 
 function AppBody() {
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior.
+        setSubmitted(true);
+    };
+
     return (
         <div className="App-body">
-            <h1>Stay updated!</h1>
-            <p>Join 60,000 + product managers receiving monthly updates on:</p>
-            <ul>
-                <li><img src={Icon} alt="Icon" />Product discovery and building what matters</li>
-                <li><img src={Icon} alt="Icon" />Measuring to ensure updates are a success</li>
-                <li><img src={Icon} alt="Icon" />And much more!</li >
-            </ul >
-            <form>
-                <label for="email">Email address<input type="text" id="email" placeholder="email@company.com" /></label>
-                <button type="submit">Subscribe to monthly newsletter</button>
-            </form>
-
-        </div >
-    )
+            {submitted ? (
+                <div>
+                    <h1>Thank you for subscribing!</h1>
+                    <p>Your email has been submitted.</p>
+                </div>
+            ) : (
+                <div>
+                    <h1>Stay updated!</h1>
+                    <p>Join 60,000+ product managers receiving monthly updates on:</p>
+                    <ul>
+                        <li>
+                            <img src={Icon} alt="Icon" />
+                            Product discovery and building what matters
+                        </li>
+                        <li>
+                            <img src={Icon} alt="Icon" />
+                            Measuring to ensure updates are a success
+                        </li>
+                        <li>
+                            <img src={Icon} alt="Icon" />
+                            And much more!
+                        </li>
+                    </ul>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="email">
+                            Email address
+                            <input type="text" id="email" placeholder="email@company.com" required />
+                        </label>
+                        <button type="submit">Subscribe to the monthly newsletter</button>
+                    </form>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default AppBody
+export default AppBody;
